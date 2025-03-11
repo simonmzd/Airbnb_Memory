e88b1dc08e2debdcefadac709a642cfb9d22fdfe
sm <mazaudsimon@gmail.com>
2025-03-11 15:00:57 +0100

    added validations

--- app/models/memorie.rb
+++ app/models/memorie.rb
@@ -1,6 +1,6 @@
 class Memorie < ApplicationRecord
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   validates :title, presence: true
   validates :image, presence: true
-  validates :description, presence: true 
+  validates :description, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,10 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
   resources :memories, only: %i[index show new create] do
-    resources :bookings, only: %i[new create destroy]
-  end 
-
-
+    resources :bookings, only: %i[new create]
+  end
+  resources :bookings, only: %i[destroy]
   # Defines the root path route ("/")
   # root "posts#index"
 end
