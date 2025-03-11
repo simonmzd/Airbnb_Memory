87bd928fbfdd88ddf2f9f1e45f494abb71ea75d6
melanie lepage <melanie.lepage86@hotmail.fr>
2025-03-11 18:38:33 +0100

    Cloudinary OK

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -13,13 +13,20 @@ class MemoriesController < ApplicationController
 
   def create
     @memorie = Memorie.new(memorie_params)
-    @memorie.save
-    redirect_to memorie_path(@memorie)
+    @memorie.user = current_user # Associe la mémoire à l'utilisateur connecté
+
+    if @memorie.save
+      redirect_to memory_path(@memorie), notice: "Souvenir créé avec succès !"
+    else
+      puts @memorie.errors.full_messages # Affiche les erreurs en console
+      flash.now[:alert] = @memorie.errors.full_messages.join(", ")
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
 
   def memorie_params
-    params.require(:memorie).permit(:title, :description, :image, :user_id)
+    params.require(:memorie).permit(:title, :description, :user_id, images: [])
   end
 end
--- app/models/memorie.rb
+++ app/models/memorie.rb
@@ -1,6 +1,7 @@
 class Memorie < ApplicationRecord
   has_many :bookings, dependent: :destroy
+  belongs_to :user
+  has_many_attached :images
   validates :title, presence: true
-  validates :image, presence: true
   validates :description, presence: true
 end
