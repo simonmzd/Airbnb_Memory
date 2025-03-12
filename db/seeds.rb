3dd0d12d0db98d70a3135615bc39f55c6efddd49
Quentin Sainton <sainton.quentin86@gmail.com>
2025-03-12 12:52:43 +0100

    DEBUG SEEDS

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,40 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
 Memorie.destroy_all
 user = User.first
 puts "Create Memories"
-Memorie.create(title: "La découverte de l'Amérique par Christophe Colomb (1492)", description: "Revivez la découverte de l'Amérique : une aventure immersive et historique !", image: "amerique.jpg", user_id: user.id)
-Memorie.create(title: "Le premier pas sur la Lune (1969) 🌕🚀", description: "Revivez le premier pas sur la Lune : une expérience lunaire inoubliable !", image: "Lune.jpg", user_id: user.id)
-Memorie.create(title: "Le couronnement de Charlemagne (800 ap. J.-C.)", description: "Revivez le couronnement de Charlemagne : une plongée historique fascinante !", image: "Charlemagne.jpg", user_id: user.id)
-Memorie.create(title: "La construction des pyramides d’Égypte (vers 2600 av. J.-C.) 🏗️🔺", description: "Revivez la construction des pyramides d’Égypte : une aventure historique captivante !", image: "Pyramide.jpg", user_id: user.id)
-Memorie.create(title: "Le sacre de Napoléon à Notre-Dame (1804) 👑", description: "Revivez le sacre de Napoléon : une cérémonie impériale grandiose !", image: "nap.jpg", user_id: user.id)
-Memorie.create(title: "L’Armistice du 11 novembre 1918 ☮️", description: "Revivez l'armistice de 1918 : un moment historique de paix !", image: "Armistice.jpg", user_id: user.id)
+memorie_1 = Memorie.create(title: "La découverte de l'Amérique par Christophe Colomb (1492)", description: "Revivez la découverte de l'Amérique : une aventure immersive et historique !", image: "amerique.jpg", user_id: user.id)
+memorie_2 = Memorie.create(title: "Le premier pas sur la Lune (1969) 🌕🚀", description: "Revivez le premier pas sur la Lune : une expérience lunaire inoubliable !", image: "Lune.jpg", user_id: user.id)
+memorie_3 = Memorie.create(title: "Le couronnement de Charlemagne (800 ap. J.-C.)", description: "Revivez le couronnement de Charlemagne : une plongée historique fascinante !", image: "Charlemagne.jpg", user_id: user.id)
+memorie_4 = Memorie.create(title: "La construction des pyramides d’Égypte (vers 2600 av. J.-C.) 🏗️🔺", description: "Revivez la construction des pyramides d’Égypte : une aventure historique captivante !", image: "Pyramide.jpg", user_id: user.id)
+memorie_5 = Memorie.create(title: "Le sacre de Napoléon à Notre-Dame (1804) 👑", description: "Revivez le sacre de Napoléon : une cérémonie impériale grandiose !", image: "nap.jpg", user_id: user.id)
+memorie_6 = Memorie.create(title: "L’Armistice du 11 novembre 1918 ☮️", description: "Revivez l'armistice de 1918 : un moment historique de paix !", image: "Armistice.jpg", user_id: user.id)
+
+
+file = File.open(File.join(Rails.root,'app/assets/images/amerique.jpg'))
+memorie_1.images.attach(io: file, filename: "nes.png", content_type: "image/png")
+memorie_1.save
+puts "memorie_1 saved"
+
+file = File.open(File.join(Rails.root,'app/assets/images/Lune.jpg'))
+memorie_2.images.attach(io: file, filename: "nes.png", content_type: "image/png")
+memorie_2.save
+puts "memorie_2 saved"
+
+file = File.open(File.join(Rails.root,'app/assets/images/Charlemagne.jpg'))
+memorie_3.images.attach(io: file, filename: "nes.png", content_type: "image/png")
+memorie_3.save
+puts "memorie_3 saved"
+
+file = File.open(File.join(Rails.root,'app/assets/images/Pyramide.jpg'))
+memorie_4.images.attach(io: file, filename: "nes.png", content_type: "image/png")
+memorie_4.save
+puts "memorie_4 saved"
+
+file = File.open(File.join(Rails.root,'app/assets/images/nap.jpg'))
+memorie_5.images.attach(io: file, filename: "nes.png", content_type: "image/png")
+memorie_5.save
+puts "memorie_5 saved"
+
+file = File.open(File.join(Rails.root,'app/assets/images/Armistice.jpg'))
+memorie_6.images.attach(io: file, filename: "nes.png", content_type: "image/png")
+memorie_6.save
+puts "memorie_6 saved"
