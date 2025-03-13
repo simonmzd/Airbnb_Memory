22e528a28ae07320d536d10c55c1e3bfa38c8a9f
melanie lepage <melanie.lepage86@hotmail.fr>
2025-03-13 14:43:51 +0100

    Dashboard + Booking => Back ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,14 @@
 Memorie.destroy_all
-user = User.first
+mel = User.create(email: "melanie.lepage86@hotmail.fr", password: "Mémory")
+simon = User.create(email: "mazaudsimon@gmail.com", password: "memory")
+quentin = User.create(email: "sainton.quentin86@gmail.com", password: "Memory")
 puts "Create Memories"
-memorie_1 = Memorie.create(title: "La découverte de l'Amérique par Christophe Colomb (1492)", description: "Revivez la découverte de l'Amérique : une aventure immersive et historique !", image: "amerique.jpg", user_id: user.id)
-memorie_2 = Memorie.create(title: "Le premier pas sur la Lune (1969) 🌕🚀", description: "Revivez le premier pas sur la Lune : une expérience lunaire inoubliable !", image: "Lune.jpg", user_id: user.id)
-memorie_3 = Memorie.create(title: "Le couronnement de Charlemagne (800 ap. J.-C.)", description: "Revivez le couronnement de Charlemagne : une plongée historique fascinante !", image: "Charlemagne.jpg", user_id: user.id)
-memorie_4 = Memorie.create(title: "La construction des pyramides d’Égypte (vers 2600 av. J.-C.) 🏗️🔺", description: "Revivez la construction des pyramides d’Égypte : une aventure historique captivante !", image: "Pyramide.jpg", user_id: user.id)
-memorie_5 = Memorie.create(title: "Le sacre de Napoléon à Notre-Dame (1804) 👑", description: "Revivez le sacre de Napoléon : une cérémonie impériale grandiose !", image: "nap.jpg", user_id: user.id)
-memorie_6 = Memorie.create(title: "L’Armistice du 11 novembre 1918 ☮️", description: "Revivez l'armistice de 1918 : un moment historique de paix !", image: "Armistice.jpg", user_id: user.id)
+memorie_1 = Memorie.create(title: "La découverte de l'Amérique par Christophe Colomb (1492)", description: "Revivez la découverte de l'Amérique : une aventure immersive et historique !", image: "amerique.jpg", user: mel)
+memorie_2 = Memorie.create(title: "Le premier pas sur la Lune (1969) 🌕🚀", description: "Revivez le premier pas sur la Lune : une expérience lunaire inoubliable !", image: "Lune.jpg", user: simon)
+memorie_3 = Memorie.create(title: "Le couronnement de Charlemagne (800 ap. J.-C.)", description: "Revivez le couronnement de Charlemagne : une plongée historique fascinante !", image: "Charlemagne.jpg", user: quentin)
+memorie_4 = Memorie.create(title: "La construction des pyramides d’Égypte (vers 2600 av. J.-C.) 🏗️🔺", description: "Revivez la construction des pyramides d’Égypte : une aventure historique captivante !", image: "Pyramide.jpg", user: mel)
+memorie_5 = Memorie.create(title: "Le sacre de Napoléon à Notre-Dame (1804) 👑", description: "Revivez le sacre de Napoléon : une cérémonie impériale grandiose !", image: "nap.jpg", user: simon)
+memorie_6 = Memorie.create(title: "L’Armistice du 11 novembre 1918 ☮️", description: "Revivez l'armistice de 1918 : un moment historique de paix !", image: "Armistice.jpg", user: quentin)
 
 
 file = File.open(File.join(Rails.root,'app/assets/images/amerique.jpg'))
