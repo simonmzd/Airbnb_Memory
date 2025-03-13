ab5c6951327a9c17c4cce2e6dc8f451c96e4c98b
Quentin Sainton <sainton.quentin86@gmail.com>
2025-03-13 11:24:44 +0100

    JAVA OK

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,12 +8,16 @@ document.addEventListener('DOMContentLoaded', function() {
   const fileInput = document.getElementById('memorie_images');
   const fileLabel = document.getElementById('file-count-label');
 
-  fileInput.addEventListener('change', function() {
-      const fileCount = fileInput.files.length;
-      if (fileCount > 0) {
-          fileLabel.textContent = `${fileCount} fichier${fileCount > 1 ? 's' : ''} sélectionné${fileCount > 1 ? 's' : ''}`;
-      } else {
-          fileLabel.textContent = 'Aucun fichier choisi';
-      }
-  });
+  if (fileInput && fileLabel) {
+      fileInput.addEventListener('change', function() {
+          const fileCount = fileInput.files.length;
+          if (fileCount > 0) {
+              fileLabel.textContent = `${fileCount} fichier${fileCount > 1 ? 's' : ''} sélectionné${fileCount > 1 ? 's' : ''}`;
+          } else {
+              fileLabel.textContent = 'Aucun fichier choisi';
+          }
+      });
+  } else {
+      console.error('Éléments #memorie_images ou #file-count-label non trouvés.');
+  }
 });
