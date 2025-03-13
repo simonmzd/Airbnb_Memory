f5c0a8c52832f4dc3a146bc306b61673406db363
Quentin Sainton <sainton.quentin86@gmail.com>
2025-03-13 10:57:08 +0100

    NOMBRE FICHIER OK

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,17 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+
+document.addEventListener('DOMContentLoaded', function() {
+  const fileInput = document.getElementById('memorie_images');
+  const fileLabel = document.getElementById('file-count-label');
+
+  fileInput.addEventListener('change', function() {
+      const fileCount = fileInput.files.length;
+      if (fileCount > 0) {
+          fileLabel.textContent = `${fileCount} fichier${fileCount > 1 ? 's' : ''} sélectionné${fileCount > 1 ? 's' : ''}`;
+      } else {
+          fileLabel.textContent = 'Aucun fichier choisi';
+      }
+  });
+});
